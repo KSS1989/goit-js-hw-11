1457cd1ebb969c969688ec79be6058b738ab4133
Serhii Klokun <99805859+KSS1989@users.noreply.github.com>
2023-04-04 23:56:01 +0300

    simple

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { fetchAll } from './partials/fetch';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const example = document.getElementById('uk');
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
@@ -11,35 +15,42 @@ function onSearch(event) {
   event.preventDefault();
   const form = event.currentTarget;
   const searchQuery = form.elements.searchQuery.value.trim();
-
+  if (searchQuery === '') {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
   fetchAll(searchQuery).then(function (data) {
     console.log(data.hits);
     renderGallery(data.hits);
+    new SimpleLightbox('.gallery a');
   });
 }
 
 function renderGallery(elements) {
   const markup = elements
     .map(element => {
-      return `<div class="photo-card">
-    <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b><br> ${element.likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b><br> ${element.views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b><br> ${element.comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b><br> ${element.comments}
-      </p>
+      return `<a href="${element.largeImageURL}">
+      <div class="photo-card">
+        <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy"/>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b><br> ${element.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b><br> ${element.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b><br> ${element.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b><br> ${element.comments}
+        </p>
+      </div>
     </div>
-    </div>
-  </div>`;
+    </a>`;
     })
     .join('');
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
--- src/partials/fetch.js
+++ src/partials/fetch.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+import Notiflix from 'notiflix';
 
 const BASE_URL = 'https://pixabay.com/api';
 const KEY_API = '34998740-5263be479f93eb60e1ee91a90';
@@ -9,6 +10,11 @@ export const fetchAll = name => {
       `${BASE_URL}/?key=${KEY_API}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
     )
     .then(function (response) {
+      if (response.data.total === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
       return response.data;
     })
     .catch(function (error) {
